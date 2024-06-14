bb662656871fffa089472c224794ac37c0ae0247
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-06-14 12:01:58 +0530

    longest-sub-array-with-sum-k

---
+++ array/longest-sub-array-with-sum-k.js
@@ -0,0 +1,34 @@
+function lenOfLongSubarr(nums,n, k){
+    //code here
+
+let hashMap = new Map()
+
+let sum = 0;
+let largest = 0;
+
+for(let i=0; i<n; i++){
+    sum+=nums[i];
+
+    if(sum === k){
+        largest = Math.max(largest, i+1)
+    }
+
+    let rem = sum - k;
+
+    if(hashMap.has(rem)){
+        let len = i - hashMap.get(rem);
+        largest = Math.max(len, largest)
+    }
+
+    if(!hashMap.has(sum)){
+        hashMap.set(sum, i)
+    }
+}
+
+return largest;
+}
+
+let nums = [10, 5, 2, 7, 1, 9];
+let n = nums.length;
+let k = 15;
+console.log(lenOfLongSubarr(nums, n, k)); // 4
