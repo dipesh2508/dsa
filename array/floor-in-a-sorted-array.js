d506bab5e118b0a9686d05da70f51c03db25797a
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-06-22 19:23:51 +0530

    binary search: floor-in-a-sorted-array

--- array/floor-in-a-sorted-array.js
+++ array/floor-in-a-sorted-array.js
@@ -2,7 +2,7 @@
 // Time complexity: O(n)
 // Space complexity: O(1)
 function findFloor(arr, n, x) {
-    //your code here
+  //your code here
   if (arr[0] > x) {
     return -1;
   }
@@ -18,6 +18,31 @@ function findFloor(arr, n, x) {
   return n - 1;
 }
 
-//driver code 
+// binary search
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+function findFloor(arr, n, x) {
+  //your code here
+  let low = 0;
+  let high = n - 1;
+  let res = -1;
+
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+
+    if (arr[mid] == x) {
+      return mid;
+    } else if (arr[mid] < x) {
+      res = mid;
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return res;
+}
+
+//driver code
 console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 7, 5)); // 1
 console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 7, 20)); // 6
