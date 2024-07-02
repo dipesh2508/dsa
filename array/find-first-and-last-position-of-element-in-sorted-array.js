1d36699a332743410971ad16edc39496a6e71bfe
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-07-02 16:34:16 +0530

    bs:find-first-and-last-position-of-element-in-sorted-array

--- array/find-first-and-last-position-of-element-in-sorted-array.js
+++ array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -23,6 +23,59 @@ var searchRange = function(nums, target) {
     return [f, l]
 };
 
+//binary search approach
+// Time complexity: O(log n)
+// Space complexity: O(1)
+function lowerBound(arr, n, x) {
+    let low = 0, high = n - 1;
+    let ans = n;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] >= x) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+function upperBound(arr, n, x) {
+    let low = 0, high = n - 1;
+    let ans = n;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] > x) {
+            ans = mid;
+            high = mid - 1;
+            console.log(ans)
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    let lb = lowerBound(nums, nums.length, target);
+    let ub = upperBound(nums, nums.length, target);
+    if(lb === nums.length || nums[lb]!==target){
+        return [-1, -1];
+    }
+
+    return [lb, ub-1];
+};
+
 //driver code
 
 let arr = [5,7,7,8,8,10];
