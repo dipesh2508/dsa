0a48d30dcb28cbd9a2ceae8f6f945e255b0e43c8
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-10-12 00:03:32 +0530

    feat:plus-one

--- array/plus-one.js
+++ array/plus-one.js
@@ -1,19 +1,38 @@
-var plusOne = function(digits) {
-    let l = digits.length - 1;
-    
-    let rec = function(p) {
-        if (digits[p] < 9) {
-            digits[p] += 1;
+//time complexity: O(n)
+//space complexity: O(1)
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    let n = digits.length - 1;
+
+    function modifyLast(x) {
+        if(digits[x]<9){
+            digits[x]+=1
             return digits;
-        } else {
-            digits[p] = 0;
-            if (p === 0) {
-                digits.unshift(1);
-                return digits;
-            }
-            return rec(p - 1); 
         }
-    };
-    
-    return rec(l);
+        digits[x] = 0;
+        if (x === 0) {
+            digits.unshift(1)
+            return digits;
+        }
+        return modifyLast(x - 1)
+    }
+    if (digits[n] === 9) {
+        return modifyLast(n)
+    } else {
+        ans = digits.map((val, index) => {
+            if (index === n) {
+                return val + 1
+            }
+            return val
+        })
+        return ans;
+    }
 };
+
+//driver code
+console.log(plusOne([1, 2, 3])); // [1,2,4]
+console.log(plusOne([4, 3, 2, 1])); // [4,3,2,2]
+console.log(plusOne([9])); // [1,0]
